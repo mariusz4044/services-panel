38c1b43e684cb2ad578b27194e4651e27c313761
mariusz4044 <mariusz4044@gmail.com>
2024-07-15 17:19:15 +0200

    refactor

---
+++ server/utils/makeID.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (length) => {
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
+//# sourceMappingURL=makeID.js.map
